75d384fce4b19cb7607ae3af1e57fabb122d73bd
biancaj08 <biancajgoodwin@gmail.com>
2026-07-12 11:39:46 -0400

    profile created

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,26 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  let name = 'Bianca';
+  let age = 27;
+  let hobbies = ['playing guitar', 'reading', 'jogging'];
+
   return (
+    // **Return JSX** includes:
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>All About {name}</h1>
+      <p>
+        {' '}
+        My name is {name} and I am currently {age} years old! I am learning
+        React and enjoy {hobbies[0]} in my free time. The full list of my
+        hobbies are the following:{' '}
+      </p>
+      <ul>
+        {' '}
+        {hobbies.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}{' '}
+      </ul>
     </div>
   );
 }
